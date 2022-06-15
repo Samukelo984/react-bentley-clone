8fa70a2ef038231b4eb9b17f86e7d33f9effc6c0
Samukelo <104824785+Samukelo984@users.noreply.github.com>
2022-06-15 12:04:32 +0200

    restructured code

--- src/Components/Homepage/Home.js
+++ src/Components/Homepage/Home.js
@@ -1,40 +1,16 @@
 import "./Home.css"; 
 import React from "react";
+import {Route} from "react-router-dom"; 
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
-        <div className="navbar">
-          <div className="logo">
-            <a href=""> 
-            <img 
-            src="https://www.bentleymotors.com/etc.clientlibs/bentley/clientlibs/bentley.motors.apps.base/resources/img/bentley_logo_a-c1@2x.png" 
-            /> 
-            </a>
-          </div> 
-
-
-          <div className="more-info-center">
-            <div className="models">
-              <span id="nav-text-one">MODELS</span>
-              <ul></ul>
-               </div>
-
-            <div className="world-ob">
-              <span id="nav-text-one">WORLD OF BENTLEY</span>
-              <ul></ul>
-               </div>
-
-            </div> 
-
-
-          <div className="more-info-right">
-            <a id="nav-text-two" href=""> CONFIGURATOR </a>
-            <a id="nav-text-two" href=""> LOACATE DEALER</a> 
-          </div>
-        </div>
+        <Route>
+          <Navbar /> 
+        </Route>
     </div>
   )
-};    
+};     
 
 export default Home; 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react"; 
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+          <div className="logo">
+            <a href=""> 
+            <img 
+            src="https://www.bentleymotors.com/etc.clientlibs/bentley/clientlibs/bentley.motors.apps.base/resources/img/bentley_logo_a-c1@2x.png" 
+            /> 
+            </a>
+          </div> 
+
+
+          <div className="more-info-center">
+            <div className="models">
+              <span id="nav-text-one">MODELS</span>
+              <ul></ul>
+               </div>
+
+            <div className="world-ob">
+              <span id="nav-text-one">WORLD OF BENTLEY</span>
+              <ul></ul>
+               </div>
+
+            </div> 
+
+
+          <div className="more-info-right">
+            <a id="nav-text-two" href=""> CONFIGURATOR </a>
+            <a id="nav-text-two" href=""> LOACATE DEALER</a> 
+          </div>
+        </div> 
+    </div>
+  ) 
+}
+
+export default Navbar; 
