4d0f409034a9e9d50157cbf59780695e755f0edb
Samukelo <104824785+Samukelo984@users.noreply.github.com>
2022-06-15 11:46:17 +0200

    added navbar elements

--- src/Components/Homepage/Home.js
+++ src/Components/Homepage/Home.js
@@ -4,7 +4,35 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-        <h1>HOMEPAGE</h1> 
+        <div className="navbar">
+          <div className="logo">
+            <a href=""> 
+            <img 
+            src="https://www.bentleymotors.com/etc.clientlibs/bentley/clientlibs/bentley.motors.apps.base/resources/img/bentley_logo_a-c1@2x.png" 
+            /> 
+            </a>
+          </div> 
+
+
+          <div className="more-info-center">
+            <div className="models">
+              <span id="nav-text-one">MODELS</span>
+              <ul></ul>
+               </div>
+
+            <div className="world-ob">
+              <span id="nav-text-one">WORLD OF BENTLEY</span>
+              <ul></ul>
+               </div>
+
+            </div> 
+
+
+          <div className="more-info-right">
+            <a id="nav-text-two" href=""> CONFIGURATOR </a>
+            <a id="nav-text-two" href=""> LOACATE DEALER</a> 
+          </div>
+        </div>
     </div>
   )
 };    
