7b955531f26247dbd86359848f30dde3a7502a04
Samukelo <104824785+Samukelo984@users.noreply.github.com>
2022-06-15 11:21:31 +0200

    added redirectory funcionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
 import "./App.css";
+import {Redirect, Route, Switch} from "react-router-dom"; 
+import Home from "./Components/Homepage/Home" 
 
-function App() {
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div> 
+      <Switch> 
+
+      <Route path="/" exact>
+        <Redirect to ="/home" />
+        </Route> 
+
+        <Route path="/home"> 
+        <Home /> 
+      </Route>
+
+      </Switch>
+    </div>  
   );
-}
+}  
 
 export default App;
